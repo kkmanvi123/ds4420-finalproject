606c4df43baa20bddffe390f013fa10aa29349c6
Annika Salpukas <annikasalpukas@gmail.com>
2026-04-15 22:46:24 -0400

    updated driver

--- cf/driver.py
+++ cf/driver.py
@@ -1,25 +1,27 @@
 import argparse
+import pandas as pd
 
 from cf import CollaborativeFiltering as CF
 
 def get_args():
     parser = argparse.ArgumentParser(description="Script to run CF algorithm on a user of interest")
-    parser.add_argument("-d", "--data", help="Path to CSV file containing patient data")
-    parser.add_argument("-u", "--user", help="User and visitation id formatted as 'user_visit'")
-    parser.add_argument("-m", "--metric", help="Metric to calculate similarity scores, one of (cosine, l2)")
-    parser.add_argument("-k", help="Value of k nearest patients for predictions (integer)")
+    parser.add_argument("-d", "--data", help="Path to CSV file containing patient data", required=True)
+    parser.add_argument("-u", "--user", help="User and visitation id formatted as 'user_visit'", required=True)
+    parser.add_argument("-m", "--metric", help="Metric to calculate similarity scores, one of (cosine, l2)", default="l2")
+    parser.add_argument("-k", help="Value of k nearest patients for predictions (integer)", default=11)
 
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    model = CF(data=args.data)
-    preds = model.run(target=args.user, metric="cosine", k=5, mask_idx=12)
+    df = pd.read_csv(args.data, index_col=0)
+    model = CF(data=df)
+    preds = model.run(target=args.user, metric=args.metric, k=args.k)
     print(f"Model predictions for patient_visit {args.user}")
     print("-"*100)
     for feat, val in preds.items():
-        print(feat, ":\t", val, "\n")
+        print(f"{feat}: \t{val}\n")
 
 if __name__ == "__main__":
     main()
